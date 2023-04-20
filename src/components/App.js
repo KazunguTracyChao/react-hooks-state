3157b00f33822e65ce4cf2ae22d231db16131efb
PhoebeTracyChaoKazungu <kazungutracy711@gmail.com>
2023-04-20 15:42:48 +0300

    Completed assignment

---
+++ src/components/App.js
@@ -0,0 +1,27 @@
+import React,{useState} from "react";
+import ShoppingList from "./ShoppingList";
+import itemData from "../data/items";
+
+function App() {
+
+  // replace 'false' with a state variable that can be toggled between true and false
+  // this will be used for the Dark Mode Toggle feature
+  const [appClass, setAppClass] = useState(true)
+
+  const handleClick = ()=>{
+    setAppClass(!appClass)
+  }
+ 
+
+  return (
+    <div onClick={handleClick} className={appClass ? "App light":"App dark"}>
+      <header>
+        <h2>Shopster</h2>
+        <button>{appClass ? 'Light Mode':'Dark Mode'}</button>
+      </header>
+      <ShoppingList items={itemData} />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Item.js
@@ -0,0 +1,21 @@
+import React,{useState}  from "react";
+
+function Item({ name, category }) {
+
+  const [inCart, setCart] = useState(false)
+
+  const addItemToCart = ()=>{
+    setCart(true);
+    
+  }
+
+  return (
+    <li className={inCart ? 'in-cart': ''}>
+      <span>{name}</span>
+      <span className="category">{category}</span>
+      <button onClick={addItemToCart} className="add">Add to Cart</button>
+    </li>
+  );
+}
+
+export default Item;
